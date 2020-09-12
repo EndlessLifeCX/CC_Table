af94856836d4bb424d2a7f91b70a1781b31697b5
EndlessLifeCX <teolastwhisper@gmail.com>
2020-09-12 15:54:45 +0300

    first commit

---
+++ src/components/v-table-row.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "v-table-row"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/v-table.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="v-table">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'v-table',
+  props: {
+    currency_data : {
+      type : Array,
+      default : ()=>{
+        return []
+      }
+    },
+},
+ data(){
+    return{}},
+  computed:{}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
